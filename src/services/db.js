6482e6d8518eb7489b892c1e32d6e22126ca44ee
Manish Sonawane <manishsonawane3010@gmail.com>
2025-07-17 07:55:01 +0000

    i have given you a mongoDB_URI in env file please check it,
    please cre

---
+++ src/services/db.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env"
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections from multiplying during
+ * development since this file might be re-executed.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function _db() {
+  if (cached.conn) {
+    console.log("🚀 Using cached MongoDB connection");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("✅ New MongoDB connection established");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
