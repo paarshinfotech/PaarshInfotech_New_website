314893b71b9410e820b20e50ebce4dbce89dba8d
Shubham Vanarse <100429473+shubs03@users.noreply.github.com>
2025-07-29 17:15:13 +0530

    Fixed : next config image issue

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'paarshedu.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
