bedb1540f00afa6c4b72f0c6fad7303927e90bb6
Manish Sonawane <manishsonawane3010@gmail.com>
2025-08-06 06:56:45 +0000

    the category based tab is not working on media page, i want to make it w

--- src/components/media/EventCategoryTabs.tsx
+++ src/components/media/EventCategoryTabs.tsx
@@ -1,6 +1,6 @@
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import {useGetMediaItemsQuery} from "@/services/api";
 
 interface EventCategoryTabsProps {
   categories: readonly string[];
@@ -8,16 +8,11 @@ interface EventCategoryTabsProps {
   setActiveCategory: (category: string) => void;
 }
 
-
-
 export default function EventCategoryTabs({
   categories,
   activeCategory,
   setActiveCategory,
 }: EventCategoryTabsProps) {
-
-  const { data: mediaItems, isLoading } = useGetMediaItemsQuery(undefined);
-  console.log("Media Items : ", mediaItems);
 
   return (
     <section className="py-8 bg-secondary border-b">
@@ -30,7 +25,7 @@ export default function EventCategoryTabs({
               onClick={() => setActiveCategory(category)}
               className="capitalize"
             >
-              {category.toLowerCase()}
+              {category.replace(/_/g, ' ')}
             </Button>
           ))}
         </div>
