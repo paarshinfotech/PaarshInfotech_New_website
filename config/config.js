aeaebedc09967dab402fa91517d1de24d188e157
Manish Sonawane <manishsonawane3010@gmail.com>
2025-08-08 05:29:20 +0000

    add new entry in config.js file for Api key and use the api key from tha

--- config/config.js
+++ config/config.js
@@ -1,1 +1,2 @@
 export const MONGODB_URI = process.env.MONGODB_URI;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import { GEMINI_API_KEY } from '../../config/config.js';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: GEMINI_API_KEY})],
   model: 'googleai/gemini-2.0-flash',
 });
