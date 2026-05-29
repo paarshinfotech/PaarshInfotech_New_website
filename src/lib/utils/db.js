5ecd3161df91c1429f01606d497836bf47de67a9
juili <juiware30@gmail.com>
2026-05-29 14:57:09 +0530

    fixed: db issues

--- src/lib/utils/db.js
+++ src/lib/utils/db.js
@@ -1,14 +1,44 @@
 import { MONGODB_URI } from "../../../config/config";
 import mongoose from "mongoose";
 
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const _db = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
+  if (mongoose.connection.readyState >= 1) {
+    return mongoose.connection;
   }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongooseInstance) => {
+      console.log("MongoDB connected");
+      return mongooseInstance;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 export default _db;
